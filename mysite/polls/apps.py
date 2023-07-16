b0fe1d1002622bdfebfcd953e56e93ed761ce7ee
Behzad Nazari <125150799+nazari1369@users.noreply.github.com>
2023-07-16 03:59:23 +0000

    1

---
+++ mysite/polls/apps.py
@@ -0,0 +1,12 @@
+# from django.apps import AppConfig
+
+
+# class PollsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'polls'
+
+from django.apps import AppConfig
+
+
+class QuizConfig(AppConfig):
+    name = 'polls'
